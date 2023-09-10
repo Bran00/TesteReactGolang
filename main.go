fce0450bb754fcda223e22f23601bb58f0548f83
Hildebrando Mendes <brandorocha00@gmail.com>
2023-09-10 15:16:59 -0400

    fix: remove dotenv again

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"os"
 
 	// "github.com/gin-contrib/cors" // Comente ou remova esta linha
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"

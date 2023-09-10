33258c271db354029850a789864c96cdf1f1859d
Hildebrando Mendes <brandorocha00@gmail.com>
2023-09-10 09:54:46 -0400

    fix: remove imports

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
